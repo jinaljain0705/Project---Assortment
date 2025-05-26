b6348d2c847de992173af12b5966351880514c12
Jinal Jain <jinaljain624@gmail.com>
2025-05-26 15:39:15 +0530

    new commit

--- Negative-Element.cpp
+++ Negative-Element.cpp
@@ -7,24 +7,24 @@ using namespace std;
 int main() {
     int n;
 
-    cout << "Enter the number of elements in the array: ";
+    cout << "Enter the number of elements in the array: " << endl;
     cin >> n;
 
     int arr[100];  
 
-    cout << "Enter the elements:";
+    cout << "Enter the elements:" << endl;
     for (int i = 0; i < n; ++i) {
         cout << "Element " << i << ": ";
         cin >> arr[i];
     }
     
-    cout << "The array is:";
+    cout << "The array is:" << endl;
     for (int i = 0; i < n; ++i) {
         cout << arr[i] << " ";
     }
     cout << endl;
 
-    cout << "Negative elements in the array are:";
+    cout << "Negative elements in the array are:" << endl;
     bool found = false;
     for (int i = 0; i < n; ++i) {
         if (arr[i] < 0) {
